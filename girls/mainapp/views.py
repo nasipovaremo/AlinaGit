69c8633f801800bb1dadcf339b01b0fb357ec11d
nasipovaremo <nasipovaown@gmail.com>
2023-12-04 13:01:25 +0600

    start project

---
+++ girls/mainapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, '')
+
+def about(request):
+    return HttpResponse("<h4>Do work<h4>")
